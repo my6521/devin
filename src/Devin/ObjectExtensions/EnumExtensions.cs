0415c2bc1efdbb7c069f301c51df601e8cb96835
wwb <22379099@qq.com>
2024-05-21 14:28:45 +0800

    enum

--- src/Devin/ObjectExtensions/EnumExtensions.cs
+++ src/Devin/ObjectExtensions/EnumExtensions.cs
@@ -1,43 +1,9 @@
-﻿using System.Collections.Concurrent;
-using System.ComponentModel;
-using System.Reflection;
-
-namespace Devin.Utitlies.Extensions
+﻿namespace Devin.Utitlies.Extensions
 {
     /// <summary>
     /// 枚举拓展类
     /// </summary>
     public static class EnumExtensions
     {
-        //缓存
-        private static ConcurrentDictionary<Enum, Tuple<int, string>> caches = new ConcurrentDictionary<Enum, Tuple<int, string>>();
-
-        /// <summary>
-        /// 获取枚举描述
-        /// </summary>
-        /// <param name="errorCode"></param>
-        /// <returns></returns>
-        public static Tuple<int, string> GetDescription(this Enum errorCode)
-        {
-            if (caches.ContainsKey(errorCode))
-            {
-                return caches[errorCode];
-            }
-
-            var name = errorCode.ToString();
-            var type = errorCode.GetType();
-            var fieldInfo = type.GetField(name);
-            var value = ((int)type.InvokeMember(name, BindingFlags.GetField, null, null, null));
-
-            object[] objs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            if (objs == null || objs.Length == 0)
-                return new Tuple<int, string>(value, name);
-
-            var descriptionAttr = (DescriptionAttribute)objs[0];
-
-            caches[errorCode] = new Tuple<int, string>(value, descriptionAttr.Description);
-
-            return caches[errorCode];
-        }
     }
 }
