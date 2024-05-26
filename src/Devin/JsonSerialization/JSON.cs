5408195c1e1c4321a5b70364d4eefe0613860b05
wwb <22379099@qq.com>
2024-05-26 12:41:30 +0800

    json

---
+++ src/Devin/JsonSerialization/JSON.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace Devin.JsonSerialization
+{
+    /// <summary>
+    /// JSON 静态帮助类
+    /// </summary>
+    public static class JSON
+    {
+        /// <summary>
+        /// 检查 JSON 字符串是否有效
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public static bool IsValid(string jsonString)
+        {
+            if (string.IsNullOrWhiteSpace(jsonString))
+            {
+                return false;
+            }
+
+            try
+            {
+                using (JsonDocument.Parse(jsonString))
+                {
+                    return true;
+                }
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+        }
+    }
+}
