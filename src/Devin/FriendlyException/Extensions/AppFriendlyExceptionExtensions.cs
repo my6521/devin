2882639003f449499e2335c1cd6625e955ecd0a4
wwb <22379099@qq.com>
2024-05-29 15:57:57 +0800

    exception

---
+++ src/Devin/FriendlyException/Extensions/AppFriendlyExceptionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Devin.FriendlyException.Extensions
+{
+    /// <summary>
+    /// 异常拓展
+    /// </summary>
+    public static class AppFriendlyExceptionExtensions
+    {
+        /// <summary>
+        /// 设置异常状态码
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="statusCode"></param>
+        /// <returns></returns>
+        public static AppFriendlyException StatusCode(this AppFriendlyException exception, int statusCode = StatusCodes.Status500InternalServerError)
+        {
+            exception.StatusCode = statusCode;
+            return exception;
+        }
+
+        /// <summary>
+        /// 设置额外数据
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static AppFriendlyException WithData(this AppFriendlyException exception, object data)
+        {
+            exception.Data = data;
+            return exception;
+        }
+    }
+}
