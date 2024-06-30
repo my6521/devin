4fc5a38f51c4cdd70d9ccc2fd4121178b8ae5409
wwb <22379099@qq.com>
2024-06-30 19:12:04 +0800

    RuntimeUtil

--- src/Devin/Utitlies/RuntimeUtil.cs
+++ src/Devin/Utitlies/RuntimeUtil.cs
@@ -17,9 +17,21 @@ namespace Devin.Utitlies
 
         private static List<Assembly> GetAllAssemblies()
         {
+            string[] filters =
+            {
+                "mscorlib",
+                "netstandard",
+                "dotnet",
+                "api-ms-win-core",
+                "runtime.",
+                "System",
+                "Microsoft",
+                "Window",
+            };
+
             var list = new List<Assembly>();
             var deps = DependencyContext.Default ?? throw new ArgumentException("注入内容获取失败 位置：RuntimeUtil.GetAllAssemblies() 18-Line");
-            var libs = deps.CompileLibraries.Where(lib => lib.Type == "project");
+            var libs = deps.CompileLibraries.Where(lib => !lib.Serviceable && (lib.Type != "package" || lib.Name.Contains("Devin")) && !filters.Any(x => lib.Name.StartsWith(x)));
             foreach (var lib in libs)
             {
                 var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(lib.Name));
