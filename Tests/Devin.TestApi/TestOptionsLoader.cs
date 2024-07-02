74ff8675cd6b56c17c0ae01bba816b6a5ed1334f
王爷 <22379099@qq.com>
2024-07-03 02:05:04 +0800

    1.0.15

--- Tests/Devin.TestApi/TestOptionsLoader.cs
+++ Tests/Devin.TestApi/TestOptionsLoader.cs
@@ -1,4 +1,6 @@
-﻿using Devin.Options.Provider;
+﻿using Devin.Options.Attributes;
+using Devin.Options.Provider;
+using Devin.Swagger.Options;
 using Devin.TestApi.Options;
 
 namespace Devin.TestApi
@@ -6,5 +8,8 @@ namespace Devin.TestApi
     public class TestOptionsLoader : BaseOptionsLoader
     {
         public TestOptions TestOptions { get; set; }
+
+        [OptionsMeta("Swagger")]
+        public SwaggerOptions SwaggerOptions { get; set; }
     }
 }
--- src/Devin/Options/Attributes/OptionsMetaAttribute.cs
+++ src/Devin/Options/Attributes/OptionsMetaAttribute.cs
@@ -3,9 +3,14 @@
     /// <summary>
     /// 配置自动注入配置标注
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
     public class OptionsMetaAttribute : Attribute
     {
+        public OptionsMetaAttribute(string key)
+        {
+            Key = key;
+        }
+
         public string Key { get; set; }
     }
 }
